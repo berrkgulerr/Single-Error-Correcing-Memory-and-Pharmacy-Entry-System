f00fe544d3f0cdbe8fe5cdffab54b2cfb35f1fc7
berrkgulerr <57874982+berrkgulerr@users.noreply.github.com>
2020-10-29 21:50:00 +0300

    Add files via upload

---
+++ lab4_1.v
@@ -0,0 +1,143 @@
+`timescale 1ns / 1ps
+
+module AddParity(
+input [1:8] dataIn,
+output reg [1:12] dataOut
+);
+
+//Write your code below
+//
+//
+always@(dataIn)
+begin
+dataOut[1] = (dataIn[1] ^ dataIn[2] ^ dataIn[4] ^ dataIn[5] ^ dataIn[7]);
+dataOut[2] = (dataIn[1] ^ dataIn[3] ^ dataIn[4] ^ dataIn[6] ^ dataIn[7]);
+dataOut[3] = dataIn[1];
+dataOut[4] = (dataIn[2] ^ dataIn[3] ^ dataIn[4] ^ dataIn[8]);
+dataOut[5] = dataIn[2];
+dataOut[6] = dataIn[3];
+dataOut[7] = dataIn[4];
+dataOut[8] = (dataIn[5] ^ dataIn[6] ^ dataIn[7] ^ dataIn[8]);
+dataOut[9] = dataIn[5];
+dataOut[10] = dataIn[6];
+dataOut[11] = dataIn[7];
+dataOut[12] = dataIn[8];
+end
+
+endmodule
+
+
+
+
+module CheckParity(
+input [1:12] dataIn,
+output reg [1:8] dataOut
+);
+
+//Write your code below
+//
+//
+reg [1:4] C;
+reg [1:8] myOut;
+always@(dataIn)
+begin
+myOut[1] = dataIn[3];
+myOut[2] = dataIn[5];
+myOut[3] = dataIn[6];
+myOut[4] = dataIn[7];
+myOut[5] = dataIn[9];
+myOut[6] = dataIn[10];
+myOut[7] = dataIn[11];
+myOut[8] = dataIn[12];
+
+C[4] = (dataIn[1] ^ dataIn[3] ^ dataIn[5] ^ dataIn[7] ^ dataIn[9] ^ dataIn[11]);
+C[3] = (dataIn[2] ^ dataIn[3] ^ dataIn[6] ^ dataIn[7] ^ dataIn[10] ^ dataIn[11]);
+C[2] = (dataIn[4] ^ dataIn[5] ^ dataIn[6] ^ dataIn[7] ^ dataIn[12]);
+C[1] = (dataIn[8] ^ dataIn[9] ^ dataIn[10] ^ dataIn[11] ^ dataIn[12]);
+
+
+
+
+if(C==0 || C==1 || C==2 || C==4 || C==8||C>12)
+begin
+	dataOut = myOut;
+end
+
+else 
+begin
+	
+    dataOut = myOut;
+	 if(C==3) begin C=C-2;end
+	 else if (C==5 || C==6 || C==7) begin C=C-3; end
+	 else begin C=C-4;end
+    dataOut[C] = (1^ dataOut[C]);
+end
+
+end
+endmodule
+
+
+
+
+module RAM(
+input [7:0] dataIn, //0:read, 1:write
+input clk,
+input mode,
+input [3:0] addr,
+output reg [7:0] dataOut);
+
+//Write your code below
+//
+//
+reg [7:0] mem[15:0];
+initial begin
+mem[0] = 0;
+mem[1] = 0;
+mem[2] = 0;
+mem[3] = 0;
+mem[4] = 0;
+mem[5] = 0;
+mem[6] = 0;
+mem[7] = 0;
+mem[8] = 0;
+mem[9] = 0;
+mem[10] = 0;
+mem[11] = 0;
+mem[12] = 0;
+mem[13] = 0;
+mem[14] = 0;
+mem[15] = 0;
+end
+
+always@(posedge clk)
+begin
+	if(mode==0)
+	begin
+		dataOut = mem[addr];
+	end
+	else
+	begin
+		mem[addr] = dataIn;
+	end
+end
+
+endmodule
+
+
+
+
+module SEC_MEM(
+input [1:12] dataIn,
+input clk,
+input mode, //0:read, 1:write
+input [3:0] addr,
+output  [1:12] dataOut);
+
+//DO NOT EDIT THIS MODULE
+wire  [7:0]  ramDataIn;
+wire  [7:0]  ramDataOut;
+CheckParity CP(dataIn,ramDataIn);
+RAM RM(ramDataIn, clk, mode, addr, ramDataOut);
+AddParity AP(ramDataOut,dataOut);
+
+endmodule
---
+++ lab4_2.v
@@ -0,0 +1,254 @@
+`timescale 1ns / 1ps
+module PharmacyMem(
+    input [4:0] studentID ,
+    input clk,
+    input [1:0] mode , // 0: list, 2: checkIn, 3: delete
+    input [7:0] checkInTime,
+    output reg [4:0] listOutput,
+    output reg listBusy ,
+    output reg ready
+	);
+
+//Write your code below
+//
+//
+reg[3:0] last;
+reg[3:0] i;
+reg[4:0]stdid[10:1];
+reg[7:0]chkin[10:1];
+reg[7:0] tempt;
+reg[4:0] tempid;
+
+initial begin
+	i=0;
+	last = 0;
+	listBusy = 0;
+	stdid[1] = 0;stdid[2] = 0;stdid[3] = 0;stdid[4] = 0;stdid[5] = 0;stdid[6] = 0;stdid[7] = 0;stdid[8] = 0;stdid[9] = 0;stdid[10] = 0;
+	chkin[1] = 0;chkin[2] = 0;chkin[3] = 0;chkin[4] = 0;chkin[5] = 0;chkin[6] = 0;chkin[7] = 0;chkin[8] = 0;chkin[9] = 0;chkin[10] = 0;
+end
+
+always@(posedge clk)
+begin
+	if(mode == 2  && last!=10)begin
+		listBusy = 0;
+		last = last+1;
+		stdid[last] = studentID;
+		chkin[last] = checkInTime;
+		
+		if((stdid[10] != 0) && chkin[10] < chkin[5])begin
+			tempt = chkin[5];
+			chkin[5] = chkin[10];
+			chkin[10] = tempt;
+			tempid = stdid[5];
+			stdid[5] = stdid[10];
+			stdid[10] = tempid;
+		end
+		
+		if((stdid[9] != 0) && chkin[9] < chkin[4])begin
+			tempt = chkin[4];
+			chkin[4] = chkin[9];
+			chkin[9] = tempt;
+			tempid = stdid[4];
+			stdid[4] = stdid[9];
+			stdid[9] = tempid;
+		end
+		
+		if((stdid[8] != 0) && chkin[8] < chkin[4])begin
+			tempt = chkin[4];
+			chkin[4] = chkin[8];
+			chkin[8] = tempt;
+			tempid = stdid[4];
+			stdid[4] = stdid[8];
+			stdid[8] = tempid;
+		end
+		
+		if((stdid[7] != 0) && chkin[7] < chkin[3])begin
+			tempt = chkin[3];
+			chkin[3] = chkin[7];
+			chkin[7] = tempt;
+			tempid = stdid[3];
+			stdid[3] = stdid[7];
+			stdid[7] = tempid;
+		end
+		
+		if((stdid[6] != 0) && chkin[6] < chkin[3])begin
+			tempt = chkin[3];
+			chkin[3] = chkin[6];
+			chkin[6] = tempt;
+			tempid = stdid[3];
+			stdid[3] = stdid[6];
+			stdid[6] = tempid;
+		end
+		
+		if((stdid[5] != 0) && chkin[5] < chkin[2])begin
+			tempt = chkin[2];
+			chkin[2] = chkin[5];
+			chkin[5] = tempt;
+			tempid = stdid[2];
+			stdid[2] = stdid[5];
+			stdid[5] = tempid;
+		end
+		
+		if((stdid[4] != 0) && chkin[4] < chkin[2])begin
+			tempt = chkin[2];
+			chkin[2] = chkin[4];
+			chkin[4] = tempt;
+			tempid = stdid[2];
+			stdid[2] = stdid[4];
+			stdid[4] = tempid;
+		end
+		
+		if((stdid[3] != 0) && chkin[3] < chkin[1])begin
+			tempt = chkin[1];
+			chkin[1] = chkin[3];
+			chkin[3] = tempt;
+			tempid = stdid[1];
+			stdid[1] = stdid[3];
+			stdid[3] = tempid;
+		end
+		
+		if((stdid[2] != 0) && chkin[2] < chkin[1])begin
+			tempt = chkin[1];
+			chkin[1] = chkin[2];
+			chkin[2] = tempt;
+			tempid = stdid[1];
+			stdid[1] = stdid[2];
+			stdid[2] = tempid;
+		end
+		i=0;
+	end
+	
+	
+	
+	else if(mode==3 && last!=0)begin
+		listBusy = 0;
+		stdid[1] = stdid[last];
+		chkin[1] = chkin[last];
+		stdid[last] = 0;
+		chkin[last] = 0;
+		last = last-1;
+		if(((chkin[1] > chkin[2]) && (chkin[3] > chkin[2]) && (stdid[2] != 0)))begin
+			tempt = chkin[1];
+			chkin[1] = chkin[2];
+			chkin[2] = tempt;
+			tempid = stdid[1];
+			stdid[1] = stdid[2];
+			stdid[2] = tempid;
+		end
+		else if(((chkin[1] > chkin[2]) && (stdid[3]==0) && (stdid[2] != 0)))begin
+			tempt = chkin[1];
+			chkin[1] = chkin[2];
+			chkin[2] = tempt;
+			tempid = stdid[1];
+			stdid[1] = stdid[2];
+			stdid[2] = tempid;
+		end
+		else if((chkin[1] > chkin[3]) && (chkin[2] > chkin[3]) && (stdid[3] != 0))begin
+			tempt = chkin[1];
+			chkin[1] = chkin[3];
+			chkin[3] = tempt;
+			tempid = stdid[1];
+			stdid[1] = stdid[3];
+			stdid[3] = tempid;
+		end
+		
+		if((chkin[2] > chkin[4]) && (chkin[5] > chkin[4]) && (stdid[4] != 0))begin
+			tempt = chkin[2];
+			chkin[2] = chkin[4];
+			chkin[4] = tempt;
+			tempid = stdid[2];
+			stdid[2] = stdid[4];
+			stdid[4] = tempid;
+		end
+		else if(((chkin[2] > chkin[4]) && (stdid[5]==0) && (stdid[4] != 0)))begin
+			tempt = chkin[2];
+			chkin[2] = chkin[4];
+			chkin[4] = tempt;
+			tempid = stdid[2];
+			stdid[2] = stdid[4];
+			stdid[4] = tempid;
+		end
+		else if((chkin[2] > chkin[5]) && (chkin[4] > chkin[5]) && (stdid[5] != 0))begin
+			tempt = chkin[2];
+			chkin[2] = chkin[5];
+			chkin[5] = tempt;
+			tempid = stdid[2];
+			stdid[2] = stdid[5];
+			stdid[5] = tempid;
+		end
+		
+		if((chkin[3] > chkin[6]) && (chkin[7] > chkin[6]) && (stdid[6] != 0))begin
+			tempt = chkin[3];
+			chkin[3] = chkin[6];
+			chkin[6] = tempt;
+			tempid = stdid[3];
+			stdid[3] = stdid[6];
+			stdid[6] = tempid;
+		end
+		else if(((chkin[3] > chkin[6]) && (stdid[7]==0) && (stdid[6] != 0)))begin
+			tempt = chkin[3];
+			chkin[3] = chkin[6];
+			chkin[6] = tempt;
+			tempid = stdid[3];
+			stdid[3] = stdid[6];
+			stdid[6] = tempid;
+		end
+		else if((chkin[3] > chkin[7]) && (chkin[6] > chkin[7]) && (stdid[7] != 0))begin
+			tempt = chkin[3];
+			chkin[3] = chkin[7];
+			chkin[7] = tempt;
+			tempid = stdid[3];
+			stdid[3] = stdid[7];
+			stdid[7] = tempid;
+		end
+		
+		if((chkin[4] > chkin[8]) && (chkin[9] > chkin[8]) && (stdid[8] != 0))begin
+			tempt = chkin[4];
+			chkin[4] = chkin[8];
+			chkin[8] = tempt;
+			tempid = stdid[4];
+			stdid[4] = stdid[8];
+			stdid[8] = tempid;
+		end
+		else if(((chkin[4] > chkin[8]) && (stdid[9]==0) && (stdid[8] != 0)))begin
+			tempt = chkin[4];
+			chkin[4] = chkin[8];
+			chkin[8] = tempt;
+			tempid = stdid[4];
+			stdid[4] = stdid[8];
+			stdid[8] = tempid;
+		end
+		else if((chkin[4] > chkin[9]) && (chkin[8] > chkin[9]) && (stdid[9] != 0))begin
+			tempt = chkin[4];
+			chkin[4] = chkin[9];
+			chkin[9] = tempt;
+			tempid = stdid[4];
+			stdid[4] = stdid[9];
+			stdid[9] = tempid;
+		end
+		i=0;
+	end
+	
+	
+	
+	else if(mode == 0)begin
+		if(i==0)begin
+			ready=1;
+			listBusy = 0;
+			i=i+1;
+		end
+		if(i>0 && i<=last)begin
+			listBusy=1;
+			ready=0;
+			listOutput = stdid[i];
+			i=i+1;
+		end
+		else if(i>last)begin
+			ready = 1;
+			listBusy = 0;
+			i = 1;
+		end
+	end
+
+end
+endmodule
